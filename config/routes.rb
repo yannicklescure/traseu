762ede324639d4fb57b6a37aad89e8271c70c81a
Pierre Jaouen <pierre.jaouen2014@gmail.com>
2019-11-25 17:57:15 -0500

    Routes creer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'itineraries#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :itineraries, only: [ :new, :create, :show ]
+  get '/search', to: 'itineraries#search', as: 'search_itineraries'
+
+  resources :itinaries_cities, only: [ :destroy, :create ]
+  resources :itinaries_spots, only: [ :destroy, :create ]
+  resources :cities, only: [ :show ]
+
 end
