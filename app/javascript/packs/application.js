8c840c314fa1fcf60a857dd42a5621cba3a59c75
Pierre Jaouen <pierre.jaouen2014@gmail.com>
2019-11-28 13:28:06 -0500

    mapbox ongoing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapBox } from '../plugins/init_mapbox';
 initMapBox();
+ MapboxDirections = require('@mapbox/mapbox-gl-directions');
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -22,8 +22,8 @@ const initMapBox = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
   const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
   });
 
   const markers = JSON.parse(mapElement.dataset.markers);
