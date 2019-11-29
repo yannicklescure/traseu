8c8b28c8763db0023e6f41dc36a60e9bcfdaac1d
Pierre Jaouen <pierre.jaouen2014@gmail.com>
2019-11-29 12:33:48 -0500

    more features on mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,4 +3,4 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapBox } from '../plugins/init_mapbox';
 initMapBox();
- MapboxDirections = require('@mapbox/mapbox-gl-directions');
+
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,7 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 const mapElement = document.getElementById('map');
+// import polyline from '@mapbox/polyline';
+var polyline = require('@mapbox/polyline');
 
 const addMarkers = (map, markers) => {
   markers.forEach((marker) => {
@@ -11,25 +13,74 @@ const addMarkers = (map, markers) => {
   });
 };
 
+const pathFinder = (map, markers) => {
+  // debugger
+  // {lat: -13.5228392, lng: -71.967627,
+  //  lat:-12.0621065, lng: -77.0365256
+
+
+  // api takes long,lat;
+  const coords = `-71.967627,-13.5228392;-77.0365256,-12.0621065`;
+  const mapboxApiKey = mapElement.dataset.mapboxApiKey;
+  const url = `https://api.mapbox.com/optimized-trips/v1/mapbox/driving/${coords}?access_token=${mapboxApiKey}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // geoJSON takes LAT , LON
+      // `data.trips[0].geometry` is the Geoline String from the API
+      const polyLine = data.trips[0].geometry;
+      // reverse (Lonng and lat) to (Lat and Lon)
+      const geoJSON = polyline.decode(polyLine).map(coord => [coord[1], coord[0]] );
+
+      const options = {
+        "id": "myroute",
+        "type": "line",
+        "source": {
+          "type": "geojson",
+          "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "LineString",
+            "coordinates":
+              geoJSON
+            }
+          }
+        },
+        "layout": {
+          "line-join": "round",
+          "line-cap": "round"
+        },
+        "paint": {
+          "line-color": "#3F3D56",
+          "line-width": 3
+        }
+      };
+      map.addLayer(options);
+    })
+};
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 6, duration: 0 });
 };
+
 
 const initMapBox = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v10'
   });
 
   const markers = JSON.parse(mapElement.dataset.markers);
 
   fitMapToMarkers(map, markers);
   addMarkers(map, markers);
+  // pathfinder prototype
+  pathFinder(map, markers);
 };
 
 export { initMapBox };
