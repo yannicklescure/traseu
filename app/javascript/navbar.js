088ab4c33b6c7eb740367daa2355e364d719905a
yannick lescure <yannick.lescure@gmail.com>
2019-11-30 15:37:51 -0500

    fix navbar colors + add js target pages

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,11 +1,18 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-lewagon');
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.add('navbar-traseu-yellow');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-white');
+        console.log(navbar.className);
+        navbar.classList.add('bg-light');
+        navbar.classList.remove('navbar-traseu-yellow');
+        // navbar.className += " bg-light";
       } else {
-        navbar.classList.remove('navbar-lewagon-white');
+        navbar.classList.remove('bg-light');
+        navbar.classList.add('navbar-traseu-yellow');
+        // navbar.className -= " bg-light";
+        console.log(navbar.className);
       }
     });
   }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,13 @@
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from "../navbar";
 
-import { initUpdateNavbarOnScroll } from '../navbar';
-initUpdateNavbarOnScroll();
+const currentPage = window.location.href.split(/.*[\/]/)[1] || null;
+console.log(currentPage);
 
+if (currentPage == null) {
+  initUpdateNavbarOnScroll();
+} else {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.add('bg-light');
+  console.log(navbar.className);
+}
