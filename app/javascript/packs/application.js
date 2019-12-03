5619462524470c7787e8e500b1b54d2767d72665
yannick lescure <yannick.lescure@gmail.com>
2019-12-03 12:55:04 -0500

    homepage form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,92 +4,37 @@ import { scrollToAnchor } from "../scroll_to_anchor";
 
 // scrollTo();
 
-const bannerMain = document.querySelector('#banner-main');
-const traseuServices = document.querySelector('#traseu-services');
-const searchExperience = document.querySelector('#search-experience');
-const searchCountry = document.querySelector('#search-country');
-const searchBudget = document.querySelector('#search-budget');
-const searchDays = document.querySelector('#search-days');
-const searchResults = document.querySelector('#search-results');
-
-// searchCountry.classList.add('d-none');
-// searchBudget.classList.add('d-none');
-// searchDays.classList.add('d-none');
-
-const navbar = document.querySelector('.navbar');
 let currentPage = window.location.href.split(/.*[\/]/)[1] || null;
 console.log(currentPage);
 
-const renderHomeSearchExperience = () => {
-  // bannerMain.classList.remove('d-none');
-  // traseuServices.classList.remove('d-none');
-  // searchExperience.classList.remove('d-none');
-  // searchCountry.classList.add('d-none');
-}
+if (currentPage == null || currentPage == '#experience' || currentPage == '#country' || currentPage == '#budget' || currentPage == '#days') {
+  initUpdateNavbarOnScroll();
+  scrollToAnchor('#experience');
 
-const renderHomeSearchCountry = () => {
-  bannerMain.classList.add('d-none');
-  traseuServices.classList.add('d-none');
-  searchExperience.classList.add('d-none');
-  searchCountry.classList.remove('d-none');
-  let searchResultsBottom = document.querySelector('#search-results-0').getBoundingClientRect().top;
-  let footerTop = document.querySelector('footer').getBoundingClientRect().top;
-  document.querySelector('#search-country').style.minHeight = `${footerTop - searchResultsBottom}px`;
-  window.scrollTo(60, 0, 'smooth');
-}
-
-// const renderHomeSearchBudget = () => {
-//   searchCountry.classList.add('d-none');
-//   searchBudget.classList.remove('d-none');
-// }
-
-// if (currentPage == '#search-experience') {
-//   console.log('experience');
-//   renderHomeSearchExperience();
-// }
-
-// if (currentPage == '#search-country') {
-//   console.log('country');
-//   renderHomeSearchCountry();
-//   navbar.classList.add('bg-light');
-//   navbar.classList.remove('navbar-traseu-yellow');
-// }
-
-if (currentPage == null || currentPage == '#search-experience') {
-  // initUpdateNavbarOnScroll();
-  scrollToAnchor('#search-experience');
-
-  document.querySelector(`#search-country-link-next`).addEventListener('click', (event) => {
-    renderHomeSearchCountry();
+  const countryNextBtn = document.querySelector("#country-next");
+  countryNextBtn.addEventListener("click", (event) => {
+    console.log("country-next clicked");
+    document.querySelector("#experience").classList.add("d-none");
+    document.querySelectorAll(".homepage").forEach((element) => {
+      element.classList.add("d-none");
+    });
+    document.querySelector("#country").classList.remove("d-none");
   });
 
-  document.querySelector(`#search-experience-link-back`).addEventListener('click', (event) => {
-    renderHomeSearchExperience();
+  const budgetNextBtn = document.querySelector("#budget-next");
+  budgetNextBtn.addEventListener("click", (event) => {
+    console.log("budget-next clicked");
+    document.querySelector("#country").classList.add("d-none");
+    document.querySelector("#budget").classList.remove("d-none");
   });
 
-  document.querySelector(`#search-budget-link-next`).addEventListener('click', (event) => {
-    renderHomeSearchBudget();
+  const daysNextBtn = document.querySelector("#days-next");
+  daysNextBtn.addEventListener("click", (event) => {
+    console.log("days-next clicked");
+    document.querySelector("#budget").classList.add("d-none");
+    document.querySelector("#days").classList.remove("d-none");
   });
 
-  // document.querySelector(`#search-country-link-back`).addEventListener('click', (event) => {
-  //   renderHomeSearchCountry();
-  // });
-
-  // window.addEventListener('scroll', (event) => {
-  //   let pageYOffset = event.currentTarget.pageYOffset;
-  //   // console.log(pageYOffset);
-  //   if (searchCountry.style.display == 'block') {
-  //     bannerMain.classList.add('d-none');
-  //     traseuServices.classList.add('d-none');
-  //     let newElement = document.querySelector('#search-country');
-  //     let newElementPosition = newElement.getBoundingClientRect();
-  //     if (pageYOffset >= newElementPosition.top) {
-  //       searchResults.style.display = 'block';
-  //     } else {
-  //       searchResults.style.display = 'none';
-  //     }
-  //   }
-  // });
 } else {
   const navbar = document.querySelector('.navbar');
   navbar.classList.add('bg-light');
