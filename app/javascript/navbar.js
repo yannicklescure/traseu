840a69e36a7923c5380535f622d53c7d40a63112
yannick lescure <yannick.lescure@gmail.com>
2020-02-28 15:32:58 -0500

    minor fixes

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,5 +1,7 @@
 const initUpdateNavbarOnScroll = (elementHeight) => {
   const navbar = document.querySelector('.navbar');
+  const loginBtn = document.querySelector('#login');
+
   console.log('navbar', navbar);
   if (navbar !== undefined) {
   // navbar.style.background = "none";
@@ -10,10 +12,12 @@ const initUpdateNavbarOnScroll = (elementHeight) => {
         // console.log(navbar.className);
         navbar.classList.add('bg-light');
         navbar.classList.remove('navbar-traseu-yellow');
+        loginBtn.classList.remove('text-white');
       } else {
         navbar.classList.remove('bg-light');
         navbar.classList.add('navbar-traseu-yellow');
         // console.log(navbar.className);
+        loginBtn.classList.add('text-white');
       }
     });
   }
